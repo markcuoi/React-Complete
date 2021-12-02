a9ec2c8e47ebe46645a323bfa986aa56bbd3a45b
thanhph4n <thanhphan10397@gmail.com>
2021-12-02 14:26:35 +0700

    created sign-in component

---
+++ React Projects/crwn-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => <SignIn className="sign-in-and-sign-up" />;
+
+export default SignInAndSignUpPage;
