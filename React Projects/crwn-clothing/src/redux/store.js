fccb26972ca5969b98844b0bddf2a3d895460e24
thanhph4n <thanhphan10397@gmail.com>
2021-12-09 16:19:24 +0700

    update componets

--- React Projects/crwn-clothing/src/redux/store.js
+++ React Projects/crwn-clothing/src/redux/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { presistStore } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger]; // this way can add more middleware
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
