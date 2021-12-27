523a276059114dabb34082b943406d07488a1c80
thanhph4n <thanhphan10397@gmail.com>
2021-12-28 01:15:48 +0700

    basic setup for React Project

--- React Projects/gpt-web/src/App.js
+++ React Projects/gpt-web/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+import {
+  Footer,
+  Blog,
+  Features,
+  WhatGPT3,
+  Header,
+  Possibility,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
+
 const App = () => {
   return (
-    <div>
-      <div>GPT-3</div>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
